032388433f11d5d7cea02c8742e5fca1317a0ddc
sambulo senda <sambulosenda@sambulos-MacBook-Pro.local>
2022-10-07 14:35:52 +0100

    create user module, controller, and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config/dist';
 import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ envFilePath: '.env.development' }),
     AuthModule,
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
