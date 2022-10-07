8d66378ecccfad9ca1d6bff6a7f9c92103f1c34c
sambulo senda <sambulosenda@sambulos-MacBook-Pro.local>
2022-10-07 13:02:53 +0100

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config/dist';
+
+@Module({
+  imports: [ConfigModule.forRoot({envFilePath: '.env.development'})],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
