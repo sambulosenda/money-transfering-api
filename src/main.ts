8e0dcc40609f0625019ed1eff4cefe5086a82847
sambulo senda <sambulosenda@sambulos-MacBook-Pro.local>
2022-10-07 14:24:15 +0100

    create auth module and controller

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,19 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const PORT = process.env.PORT || 9001
+  const PORT = process.env.PORT || 9001;
+  app.enableCors({ origin: ['http://localhost:3000'], credentials: true });
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe());
   try {
-    await app.listen(PORT, () => console.log(`Runny BazzPay Money Tansfer API API on Port ${PORT}`));
+    await app.listen(PORT, () =>
+      console.log(`Runny BazzPay Money Tansfer API API on Port ${PORT}`),
+    );
   } catch (err) {
-    console.log(err)
+    console.log(err);
   }
 }
 bootstrap();
